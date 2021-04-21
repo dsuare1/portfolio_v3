0b6a7165b6f65f5c9ff6ec48585220bdd211610a
derrick <suarez.derrick@gmail.com>
2021-04-20 23:04:17 -0500

    tidy up footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -9,6 +9,7 @@ import * as routePaths from '../../constants/routePaths';
 import * as socialLinks from '../../constants/socialLinks';
 import style from './footer.module.scss';
 import navStyle from '../navbar/navbar.module.css';
+import { SecondaryCopy } from '../typography/Copy';
 
 const targetBlank = '_blank';
 
@@ -16,36 +17,41 @@ const Footer = () => {
   const { pathname } = useRouter();
 
   return (
-    <div className={style.footerContainer}>
-      <div className={style.socials}>
-        <a href={socialLinks.gitHub} target={targetBlank}>
-          <FaGithub className={style.socialIcon} />
-        </a>
-        <a href={socialLinks.linkedIn} target={targetBlank}>
-          <FaLinkedin className={style.socialIcon} />
-        </a>
-        <a href={socialLinks.twitter} target={targetBlank}>
-          <FaTwitter className={style.socialIcon} />
-        </a>
-        <a href={socialLinks.instagram} target={targetBlank}>
-          <FaInstagram className={style.socialIcon} />
-        </a>
+    <>
+      <div className={style.footerContainer}>
+        <div className={style.footerNav}>
+          <HeadingH5>
+            <Link href="/">
+              <a
+                href="/"
+                className={`${navStyle.navLink} ${
+                  pathname === routePaths.HOME ? navStyle.navLinkActive : null
+                }`}
+              >
+                Home
+              </a>
+            </Link>
+          </HeadingH5>
+        </div>
+        <div className={style.socials}>
+          <a href={socialLinks.gitHub} target={targetBlank}>
+            <FaGithub className={style.socialIcon} />
+          </a>
+          <a href={socialLinks.linkedIn} target={targetBlank}>
+            <FaLinkedin className={style.socialIcon} />
+          </a>
+          <a href={socialLinks.twitter} target={targetBlank}>
+            <FaTwitter className={style.socialIcon} />
+          </a>
+          <a href={socialLinks.instagram} target={targetBlank}>
+            <FaInstagram className={style.socialIcon} />
+          </a>
+        </div>
       </div>
-      <div className={style.footerNav}>
-        <HeadingH5>
-          <Link href="/">
-            <a
-              href="/"
-              className={`${navStyle.navLink} ${
-                pathname === routePaths.HOME ? navStyle.navLinkActive : null
-              }`}
-            >
-              Home
-            </a>
-          </Link>
-        </HeadingH5>
+      <div className={style.copyright}>
+        <SecondaryCopy>&copy; Derrick Suarez 2021</SecondaryCopy>
       </div>
-    </div>
+    </>
   );
 };
 
